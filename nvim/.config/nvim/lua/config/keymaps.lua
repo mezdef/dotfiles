9724b78b591533d308bb677721395b76271c3cf4
Marc de Ferranti <mezdef@gmail.com>
2026-02-15 23:16:22 +1100

    Nvim - Add quicker.nvim to enhance quickfix list

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- vim.keymap.del("n", "<leader>gd")
+vim.keymap.set("n", "<leader>q", function()
+  require("quicker").toggle()
+end, {
+  desc = "Toggle quickfix",
+})

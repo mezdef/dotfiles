6db3fe8a01a170453afae90e5a6f7db8af66b1cb
Marc de Ferranti <mezdef@gmail.com>
2026-02-16 15:17:50 +1100

    Nvim - Use gitsigns for inline gitblame

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,22 @@
+-- Add this to ~/.config/nvim/lua/plugins/gitsigns.lua
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+    },
+  },
+}

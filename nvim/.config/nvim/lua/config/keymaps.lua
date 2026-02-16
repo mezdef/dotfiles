a315701634cb5805ca4d7ce0494b25ab5d9266a5
Marc de Ferranti <mezdef@gmail.com>
2026-02-16 16:33:37 +1100

    Nvim - remove mini.diff

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,11 @@ vim.keymap.set("n", "<leader>q", function()
 end, {
   desc = "Toggle quickfix",
 })
+-- Diffview
+vim.keymap.set("n", "<leader>gd", function()
+  if next(require("diffview.lib").views) == nil then
+    vim.cmd("DiffviewOpen")
+  else
+    vim.cmd("DiffviewClose")
+  end
+end, { desc = "Toggle Diffview window" })
--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -3,20 +3,25 @@ return {
   "lewis6991/gitsigns.nvim",
   opts = {
     current_line_blame = true,
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
-    },
-    signs_staged = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▎" },
-    },
+    -- current_line_blame_opts = {
+    --   virt_text = true,
+    --   virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    --   delay = 1000,
+    -- },
+    -- signs = {
+    --   add = { text = "▎" },
+    --   change = { text = "▎" },
+    --   delete = { text = "" },
+    --   topdelete = { text = "" },
+    --   changedelete = { text = "▎" },
+    --   untracked = { text = "▎" },
+    -- },
+    -- signs_staged = {
+    --   add = { text = "▎" },
+    --   change = { text = "▎" },
+    --   delete = { text = "" },
+    --   topdelete = { text = "" },
+    --   changedelete = { text = "▎" },
+    -- },
   },
 }

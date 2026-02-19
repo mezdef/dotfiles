8bc66c921f62def297232e74e47fdd7a6ac9e786
Marc de Ferranti <mezdef@gmail.com>
2026-02-19 21:48:30 +1100

    Tmux - tba

---
+++ tmux/.config/tmux/scripts/jj-status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hello!"
+
+# # Run jj status
+# jj status >/dev/null 2>&1
+#
+# # Check the exit status of the last command
+# if [ $? -eq 0 ]; then
+#   echo "Jujutsu (jj) status is active."
+#   jj status
+# else
+#   echo "Error: This is not a jj repository or jj is not installed."
+#   exit 1
+# fi

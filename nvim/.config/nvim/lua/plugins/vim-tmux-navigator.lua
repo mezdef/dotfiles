ea6eda5de46060eaf1e8e0d5c0125acf6530b38a
Marc de Ferranti <mezdef@gmail.com>
2026-04-22 11:03:18 +1000

    Tmux - Migrate tmux leaderless bindings from Ctrl to Alt

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,10 +9,9 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
   },
 }
